ff1c3b584f85c8fc7304f72705439c7d52624973
rainupup <320951152@qq.com>
2022-09-08 17:59:08 +0800

    first commit

--- Module-cloud-gateway-gateway9527/src/test/java/test.java
+++
@@ -1,8 +0,0 @@
-import java.time.ZonedDateTime;
-
-public class test {
-    public static void main(String[] args) {
-        ZonedDateTime zbj = ZonedDateTime.now(); // 默认时区
-        System.out.println(zbj);
-    }
-}
